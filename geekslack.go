7bef7dbd78afae7f7c3ee01e162bc1b49a632f1d
縣美早 <>
2018-12-02 22:32:04 +0900

    画像を全てガッキーにしました

--- geekslack.go
+++ geekslack.go
@@ -56,46 +56,43 @@ func Handle(req *Request) (mes string, err error) {
 		return
 	}
 
+	if contains(string(req.Text), []string{"お疲れ", "おつかれ"}) {
+		if req.Username == "sunshine" {
+			mes = string(req.UserName) + "おっけーサンシャイン!グラスを空けて！"
+			return
+		}
+		mes = string(req.UserName) + "さん、お疲れ様！"
+		return
+	}
+
 	mes = "なに？"
 	return
 }
 
 func kannaImage() string {
 	urls := []string{
-		"https://pbs.twimg.com/profile_images/1009220670826295297/qfHNpNPM_400x400.jpg",
-		"https://upload.wikimedia.org/wikipedia/commons/thumb/b/b9/Hashimoto_Kanna_at_Opening_Ceremony_of_the_Tokyo_International_Film_Festival_2017_%2840169184582%29.jpg/220px-Hashimoto_Kanna_at_Opening_Ceremony_of_the_Tokyo_International_Film_Festival_2017_%2840169184582%29.jpg",
-		"https://dot.asahi.com/S2000/upload/2018101500008_1.jpg",
-		"https://news.walkerplus.com/article/166398/970981_615.jpg",
-		"https://img.sirabee.com/wp/wp-content/uploads/2018/03/sirabee20180318hasimotokanna1-600x400.jpg",
-		"https://iwiz-chie.c.yimg.jp/im_siggYFrYURY5uMNYh993Ack.nw---x320-y320-exp5m-n1/d/iwiz-chie/que-10188509055",
-		"http://sabziblog.com/wp-content/uploads/2018/03/568867_615.jpg",
-		"https://pbs.twimg.com/media/DBl9HtdVoAAzt1A.jpg",
-		"https://realsound.jp/wp-content/uploads/2018/03/20180305-hashimoto2-950x534.jpg",
-		"https://realsound.jp/wp-content/uploads/2017/10/20171020_kanna.jpg",
-		"https://news-sokuhou.site/wp-content/uploads/2017/10/%E6%A9%8B%E6%9C%AC%E7%92%B0%E5%A5%88%E3%81%AE%E3%83%9E%E3%83%8D%E3%83%BC%E3%82%B8%E3%83%A3%E3%83%BC%E3%81%AF%E8%AA%B0%EF%BC%9F%E5%90%8C%E5%B1%85%E3%82%82%E3%81%97%E3%81%A6%E3%81%84%E3%81%9F%E3%80%8C%E7%BE%A8%E3%81%BE%E3%81%97%E3%81%99%E3%81%8E%E3%82%8B%EF%BC%81%E3%80%8D.jpg",
-		"https://i2.wp.com/unicorntomo.com/wp-content/uploads/2018/10/hashimoto10.jpg?fit=700%2C875&ssl=1",
-		"https://img.cinematoday.jp/a/N0091045/_size_640x/_v_1492503321/main.jpg",
-		"https://bikuchan.com/wp-content/uploads/2017/07/87b742db495de5becade67ea7621bbb6.jpg",
-		"https://cdn-ak.f.st-hatena.com/images/fotolife/g/gaigar2444/20180228/20180228202908.jpg",
-		"http://スマホ壁紙無料.com/wp-content/uploads/2016/11/hasimotokanna-95-a.jpg",
-		"https://i.ytimg.com/vi/08daZMHVlFE/maxresdefault.jpg",
-		"http://image.news.livedoor.com/newsimage/1/b/1b2af63a0fb0dc78318cf4f3a49c67c7.jpg",
-		"https://grapee.jp/wp-content/uploads/45100_main.jpg",
-		"https://mykneecap.com/wp-content/uploads/2018/08/hashimotokanna_pile0022.jpg",
-		"https://news.walkerplus.com/article/163509/951502_615.jpg",
-		"https://instagram.com/p/Be933O_l52j/media/?size=l",
-		"https://o.aolcdn.com/images/dims3/GLOB/crop/2714x1359+0+707/resize/630x315!/format/jpg/quality/85/http%3A%2F%2Fo.aolcdn.com%2Fhss%2Fstorage%2Fmidas%2F57d13f080185c48549e8ef48f2520721%2F206029083%2Fjpp025325840.jpg",
-		"https://amd.c.yimg.jp/im_siggyIWiLTdp8lkTrpsgrTGcsg---x400-y267-q90-exp3h-pril/amd/20180830-00000018-flix-000-1-view.jpg",
-		"https://tak16.com/wp-content/uploads/2017/06/2017-06-07_18h21_14.png",
-		"https://img.dmenumedia.jp/ent/wp-content/uploads/2018/01/285px-Kanna_hashimoto2017.jpg",
-		"https://芸能人の実家住所まとめ.com/wp-content/uploads/2018/01/Screenshot-2018-01-13_16-32-21.png",
+		"http://xn--zck8ci9591bzonbkq1xt.com/wp-content/uploads/2018/11/aragakiyui-192-a.jpg",
+		"http://xn--zck8ci9591bzonbkq1xt.com/wp-content/uploads/2018/11/aragakiyui-193-a.jpg",
+		"http://xn--zck8ci9591bzonbkq1xt.com/wp-content/uploads/2018/11/aragakiyui-194-a.jpg",
+		"http://xn--zck8ci9591bzonbkq1xt.com/wp-content/uploads/2018/02/aragakiyui-161-a.jpg",
+		"http://xn--zck8ci9591bzonbkq1xt.com/wp-content/uploads/2018/02/aragakiyui-163-a.jpg",
+		"http://xn--zck8ci9591bzonbkq1xt.com/wp-content/uploads/2018/02/aragakiyui-164-a.jpg",
+		"http://xn--zck8ci9591bzonbkq1xt.com/wp-content/uploads/2018/02/aragakiyui-165-a.jpg",
+		"http://xn--zck8ci9591bzonbkq1xt.com/wp-content/uploads/2018/02/aragakiyui-162-a.jpg",
+		"http://xn--zck8ci9591bzonbkq1xt.com/wp-content/uploads/2018/07/aragakiyui-177-a.jpg",
+		"http://xn--zck8ci9591bzonbkq1xt.com/wp-content/uploads/2018/07/aragakiyui-180-a.jpg",
+		"http://xn--zck8ci9591bzonbkq1xt.com/wp-content/uploads/2018/07/aragakiyui-176-a.jpg",
+		"http://xn--zck8ci9591bzonbkq1xt.com/wp-content/uploads/2018/03/aragakiyui-166-a.jpg",
+		"http://xn--zck8ci9591bzonbkq1xt.com/wp-content/uploads/2018/03/aragakiyui-168-a.jpg",
+		"http://xn--zck8ci9591bzonbkq1xt.com/wp-content/uploads/2018/03/aragakiyui-169-a.jpg",
+		"http://xn--zck8ci9591bzonbkq1xt.com/wp-content/uploads/2018/03/aragakiyui-170-a.jpg",
 	}
 	return urls[rand.Intn(len(urls))]
 }
 
 func kannaWordWithImage() string {
 	replys := []string{
-		"私の画像ねー？　はい！",
+		"ガッキーの画像ねー？　はい！",
 		"いいよー、はい！",
 		"仕方ないなあ、どうぞー",
 		"彼女とデートなうに使っていいよ！",
@@ -112,7 +109,7 @@ func postImage(imageURL string) error {
 	m["text"] = kannaWordWithImage()
 	m["attachments"] = []map[string]string{
 		map[string]string{
-			"fallback":  "環奈の画像だよ！",
+			"fallback":  "ガッキーの画像だよ！",
 			"image_url": imageURL,
 		},
 	}
